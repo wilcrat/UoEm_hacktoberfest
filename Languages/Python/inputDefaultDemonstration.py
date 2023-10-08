97664d7427766d24f6ef9a14e36865e870f537ee
Winston Wacieni <winston.mureithi.i@gmail.com>
2023-10-08 19:51:42 +0300

    Added python Notes

---
+++ Languages/Python/inputDefaultDemonstration.py
@@ -0,0 +1,14 @@
+# python code to demonstrate example
+# of input() function
+
+val1 = input("Enter any value: ")
+print("value of val1: ", val1)
+print("type of val1: ", type(val1))
+
+val2 = input("Enter another value: ")
+print("value of val2: ", val2)
+print("type of val2: ", type(val2))
+
+val3 = input("Enter another value: ")
+print("value of val3: ", val3)
+print("type of val3: ", type(val3))
